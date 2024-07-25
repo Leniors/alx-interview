77454ddbe70ae2d3681460c60cf55946119c31d8
Leniors <mokualeeroy@gmail.com>
2024-07-25 03:31:51 +0300

    makeChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -32,6 +32,5 @@ def makeChange(coins, total):
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
-        break
     
     return dp[total] if dp[total] != float('inf') else -1
