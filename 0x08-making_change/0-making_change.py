e0b68b75fe88f55cd44992f36ca062b689ef52fa
Leniors <mokualeeroy@gmail.com>
2024-07-25 03:26:07 +0300

    makeChange

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+makeChange function file
+"""
+
+def makeChange(coins, total):
+    """ makeChange function """
+    if total <= 0:
+        return 0
+    
+    if total in coins:
+        return 1
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    
+    coins.sort()
+    
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+        break
+    
+    return dp[total] if dp[total] != float('inf') else -1
