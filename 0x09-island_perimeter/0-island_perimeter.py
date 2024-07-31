613f034c06623f7220db918de754a2e5898883bd
Leniors <mokualeeroy@gmail.com>
2024-08-01 01:17:04 +0300

    islan perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter.py
+"""
+
+def island_perimeter(grid):
+    """
+    inspect the grid and get its perimeter
+    """
+    
+    perimeter = 0
+    
+    for i in range(len(grid) - 1):
+        for j in range(len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                add = 4
+            else:
+                add = 0
+
+            if grid[i][j] == 1 and j > 0 and grid[i][j - 1] == 1:
+                add -= 2
+
+            if grid[i][j] == 1 and i > 0 and grid[i - 1][j] == 1:
+                add -= 2
+
+            perimeter += add
+
+    return perimeter
