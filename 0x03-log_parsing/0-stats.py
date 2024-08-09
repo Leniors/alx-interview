e843741e346eaa8920940a65a95c8e48bc2957aa
Leniors <mokualeeroy@gmail.com>
2024-08-09 05:32:14 +0300

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,76 +1,37 @@
 #!/usr/bin/python3
-'''script that reads stdin line by line and computes metrics'''
-import signal
-from sys import stdin, exit
-# from re import compile
+""" Reads `stdin` line by line and computes metrics """
 
-STATUS = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
-}
-
-
-def print_statistics(total):
-    '''print statistics'''
-    out = 'File size: {}\n'.format(total)
-    for k, v in sorted(STATUS.items()):
-        if v > 0:
-            out += '{}: {}\n'.format(k, v)
-    print(out, end='')
-
-
-def handler(signum, frame):
-    '''sginal handler'''
-    print_statistics(total)
-    exit(0)
-
-
-# signal interrupt handler
-signal.signal(signal.SIGINT, handler)
+import sys
 
 if __name__ == '__main__':
-    i = 0
-    total = 0
+
+    fileSize, counter = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    statusCount = {k: 0 for k in codes}
+
+    def print_statistics(statusCount: dict, file_size: int) -> None:
+        print("File size:", fileSize)
+        for k, v in sorted(statusCount.items()):
+            if v != 0:
+                print("{}: {}".format(k, v))
 
     try:
-        for line in stdin:
-            # regex to match log line
-            # regex = compile(
-            #         r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}'
-            #         r' - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" '
-            #         r'(\d{3}) (\d+)$'
-            #         )
-            # m = regex.match(line)
-
-            # if not m:
-            #     # there's no match skip line
-            #     continue
+        for line in sys.stdin:
+            counter += 1
+            data = line.split()
             try:
-                toks = line.split()
-                status_code, size = toks[-2], int(toks[-1])
-                # _, __, ___, status_code, size = m.groups()
-
-                if status_code in STATUS:
-                    STATUS[status_code] += 1
-                total += size
-
-                # book keeping
-                i += 1
-
-                if i % 10 == 0:
-                    # print statistic
-                    print_statistics(total)
-
-            except Exception:
+                status_code = data[-2]
+                if status_code in statusCount:
+                    statusCount[status_code] += 1
+            except BaseException:
                 pass
-
+            try:
+                fileSize += int(data[-1])
+            except BaseException:
+                pass
+            if counter % 10 == 0:
+                print_statistics(statusCount, fileSize)
+        print_statistics(statusCount, fileSize)
     except KeyboardInterrupt:
-        pass
-    finally:
-        print_statistics(total)
+        print_statistics(statusCount, fileSize)
+        raise
